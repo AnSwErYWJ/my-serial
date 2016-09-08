a63a6347a7ffa1389dce659a7869c60f8d9cd0b3
weijie.yuan <weijie.yuan@aispeech.com>
2016-09-08 16:16:23 +0800

    complete main.c

--- myft/main.c
+++ myft/main.c
@@ -5,8 +5,24 @@
 	> Created Time: Thu 08 Sep 2016 11:26:20 AM CST
  ************************************************************************/
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+#define FT_F 1 // not directory
+#define FT_D 2 // is directory
+#define FT_DNR 3 // unreadable
+#define FT_NS 4 // no stat
+
+static char *fullpath; // file path name
+static size_t pathlen; // file path lenngth
+
+typedef int Myfunc(const char *,const struct stat *,int);
+static Myfunc myfunc;
 
 /*
  * nreg：普通文件的数量； 
@@ -20,7 +36,137 @@
  */
 static long nreg, ndir, nblk, nfifo, nchr, nslink, nsock, ntot;
 
-static int myft(char *pathname,Myfunc *func)
+/* malloc for pathname*/
+static char *path_alloc(size_t *size)
+{
+    char *p = NULL;
+    if (!size)
+        return NULL;
+    p = malloc(256);
+    if (p)
+        *size = 256;
+    else
+        *size = 0;
+    
+    return p;
+}
+
+static int myfunc(const char *pathname, const struct stat *statptr, int type)
+{
+    switch (type)
+    {
+        /* not directory*/
+        case FT_F:
+            switch (statptr->st_mode & S_IFMT)
+            {
+                case S_IFREG:    
+                    nreg++;        
+                    break;
+                case S_IFBLK:    
+                    nblk++;        
+                    break;
+                case S_IFCHR:    
+                    nchr++;        
+                    break;
+                case S_IFIFO:    
+                    nfifo++;    
+                    break;
+                case S_IFLNK:    
+                    nslink++;    
+                    break;
+                case S_IFSOCK:    
+                    nsock++;    
+                    break;
+                case S_IFDIR:    
+                    printf("for S_IFDIR for %s", pathname);
+            }
+            break;
+        
+        /* is directory */
+        case FT_D:
+            ndir++;
+            break;
+
+        /* is directory, but unreadable */
+        case FT_DNR:
+            printf("%s 目录不可读", pathname);
+                break;
+            case FT_NS:
+                printf("%s stat出错", pathname);
+                break;
+            default:
+                printf("%d类型不可识别，路径名师 %s", type, pathname);
+    }
+
+    return 0;
+}
+
+
+static int dopath(Myfunc *func)
+{
+    struct stat statbuf;
+    struct dirent *dirp;
+    DIR *dp;
+    int ret, n;
+    
+    /* get stat info of file */
+    if (lstat(fullpath, &statbuf) < 0)
+        return (func(fullpath,&statbuf,FT_NS));
+
+    /* not a directory */
+    if (S_ISDIR(statbuf.st_mode) == 0)
+        return (func(fullpath,&statbuf,FT_F));
+
+    /* is a directory */
+    if ((ret = func(fullpath,&statbuf,FT_D)) != 0)
+        return ret;
+
+    /* realloc */
+    n = strlen(fullpath);
+    if (n + NAME_MAX +2 > pathlen)
+    {
+        pathlen *= 2;
+        if ((fullpath = realloc(fullpath,pathlen)) == NULL)
+            printf("realloc failed!\n");
+    }
+    fullpath[n++] = '/';
+    fullpath[n] = 0;
+
+    /* do every directories*/
+    if ((dp = opendir(fullpath)) == NULL)
+        return (func(fullpath,&statbuf,FT_DNR));
+    while ((dirp = readdir(dp)) != NULL)
+    {
+        if (strcmp(dirp->d_name,".") == 0 || strcmp(dirp->d_name,"..")==0)
+            continue; // avoid '.' and '..'
+        strcpy(&fullpath[n],dirp->d_name); // add directory name after '/'
+        if ((ret = dopath(func)) != 0)
+            break;
+    }
+    fullpath[n-1] = 0;
+
+    /* close directory */
+    if (closedir(dp) < 0)
+        printf("can't close directory %s",fullpath);
+    return (ret);
+}
+
+static int myft(const char *pathname,Myfunc *func)
+{
+    /* malloc for pathname*/
+    fullpath = path_alloc(&pathlen);
+
+    if (pathlen <= strlen(pathname))
+    {
+        pathlen = strlen(pathname) * 2;
+        if ((fullpath = realloc(fullpath,pathlen)) == NULL);
+            printf("realloc failed!\n");
+    }
+
+    strcpy(fullpath,pathname);
+
+    return (dopath(func));
+}
 
 int main(int argc,const char *argv[])
 {
