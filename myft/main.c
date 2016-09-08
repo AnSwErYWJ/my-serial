883f613b14848a067721baaa664a74f993b6b209
weijie.yuan <weijie.yuan@aispeech.com>
2016-09-08 14:23:17 +0800

    add main in myft

--- myft/main.c
+++ myft/main.c
@@ -20,14 +20,36 @@
  */
 static long nreg, ndir, nblk, nfifo, nchr, nslink, nsock, ntot;
 
+static int myft(char *pathname,Myfunc *func)
+
 int main(int argc,const char *argv[])
 {
+    int result = -1;
+
+    /* check arguments */
     if (argc != 2)
     {
         printf("Usage:\n");
         exit(EXIT_FAILURE);
     }
 
-
+    result = myft(argv[1],myfunc);
     
+    /* total numbers */
+    ntot = nreg + ndir + nblk + nfifo + nchr + nslink + nsock + ntot;
+
+    /* avoid divisor equal 0 */
+    ntot = ntot ? ntot : 1;
+
+    printf("file type        amounts  percent\n");
+    printf("regular files    %7ld  %5.2f%%\n", nreg, (nreg*100.0/ntot));
+    printf("directories      %7ld  %5.2f%%\n", ndir, (ndir*100.0/ntot));
+    printf("block special    %7ld  %5.2f%%\n", nblk, (nblk*100.0/ntot));
+    printf("char special     %7ld  %5.2f%%\n", nchr, (nchr*100.0/ntot));
+    printf("FIFOs            %7ld  %5.2f%%\n", nfifo, (nfifo*100.0/ntot));
+    printf("symbolic links   %7ld  %5.2f%%\n", nslink, (nslink*100.0/ntot));
+    printf("sockets          %7ld  %5.2f%%\n", nsock, (nsock*100.0/ntot));
+    printf("total            %7ld\n", ntot);
+    
+    return result;
 }
